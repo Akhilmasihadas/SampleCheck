7fe677afc4c0e343f7a14990e5091b103478b8ca
Akhilmasihadas <89574383+Akhilmasihadas@users.noreply.github.com>
2024-07-30 22:03:14 +0530

    initial check of committing

---
+++ Adding.java
@@ -0,0 +1,8 @@
+class Adding{
+public static void main (String[] args){
+	float f1 = 123.4567f;
+	float f2 = 321.87654f;
+	float sum = f1 +f2;
+System.out.println("Sum of 2 numbers " + f1+ " + " +f2 +" = " +sum);
+}
+}
